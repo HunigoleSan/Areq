38e062c7fbba243063d0ea3dae21e3c7c01199a3
HunigoleSan <soy.hugo.pe@gmail.com>
2023-07-01 00:55:22 -0500

    eliminando archivos js

--- script/redirecionar.js
+++
@@ -1,9 +0,0 @@
-const p_uno = document.getElementById('p_1');
-console.log()
-
-var url_location = window.location.href
-var url_location_uno = "http://127.0.0.1:5500/index.html#uno"
-var a = document.getElementById('p_1');
-
-
-
